273b5ed6b7f1be629e9aa9b4f94008ddb08cec9e
Aliaksei Iliukevich <aleks.ilykevich@gmail.com>
2023-05-15 07:01:19 +0300

    fix: design

---
+++ pages/contacts/send.php
@@ -0,0 +1,32 @@
+<?php
+
+$name = $_POST['name'];
+$tel = $_POST['tel'];
+$message = $_POST['message'];
+
+
+$name = htmlspecialchars($name);
+$tel = htmlspecialchars($tel);
+$message = htmlspecialchars($message);
+
+$name = urldecode($name);
+$tel = urldecode($tel);
+$message = urldecode($message);
+
+$name = trim($name);
+$tel = trim($tel);
+$message = trim($message);
+
+if(mail("aleks.ilykevich@gmail.com",
+        "Новое письмо с сайта!",
+        "Имя: ".$name."\n".
+        "Телефон: ".$tel."\n".
+        "Сообщение: ".$message,
+        "От:  no-reply@maria-designer.com \r\n")
+){
+    header("Location: http://designer-cv/pages/thanks/");
+} 
+else {
+    header("Location: http://designer-cv/pages/error/");
+}
+?>
